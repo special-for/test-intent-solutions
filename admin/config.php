b2c4300671115efb9d55307c7f6509a76df7a9fd
Andriy Shapran <shapran_94@mail.ru>
2020-09-27 19:19:57 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test/admin/');
+define('HTTP_CATALOG', 'http://test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test/admin/');
+define('HTTPS_CATALOG', 'http://test/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/test/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/test/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/test/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/test/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'test_oc3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
